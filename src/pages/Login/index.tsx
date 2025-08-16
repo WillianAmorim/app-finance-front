a6765b04b29e0801f1b1af64ecaf662968a2c090
Willian Amorim <willianamorim1@outlook.com.br>
2025-08-16 00:27:23 -0300

    finalizado página de login mobile

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,34 +1,38 @@
-import imageLogin from '../../assets/image-login.svg'
 import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
+import { Eye, EyeOff } from "lucide-react"
+import { useState } from "react"
 
 const Index = () => {
+    const [showPassword, setShowPassword] = useState(false);
+
     return (
-        <div className='flex h-screen'>
-            <div className='w-1/3 bg-green-500 hidden md:block'>
-                <img className='h-full w-full object-cover' src={imageLogin} alt="Description of image" />
+        <div className='flex flex-col h-screen bg-gray-100'>
+            <div className='bg-black p-4 rounded-b-xl '>
+                <h1 className='text-white text-center'>Finance</h1>
             </div>
 
-            <div className='w-2/3 flex justify-center items-center'>
-                <div className='bg-white p-8 rounded-lg shadow-lg w-96'>
-                    <h1 className='text-2xl font-bold mb-4'>Login</h1>
-                    <form className='space-y-4'>
-                        <div>
-                            <Label htmlFor="email">Email</Label>
-                            <Input id="email" type="email" placeholder="Digite seu email" />
+            <div className="flex flex-1 items-center justify-center">
+                <div className="p-6 rounded-lg shadow-lg w-80 bg-white">
+                    <h1 className="text-black pb-4 text-xl font-semibold">Login</h1>
+                    <div className="flex flex-col gap-4">
+                        <div className='flex flex-col gap-2'>
+                            <Label>Email</Label>
+                            <Input className='' type="email" />
                         </div>
-                        <div>
-                            <Label htmlFor="password">Senha</Label>
-                            <Input id="password" type="password" placeholder="Digite sua senha" />
+                        <div className='flex flex-col gap-2'>
+                            <Label>Password</Label>
+                            <div className='border border-gray-300 rounded-md flex items-center bg-white'>
+                                <Input className=" border-none" type={showPassword ? "text" : "password"} />
+                                <div onClick={() => setShowPassword(!showPassword)} className=" px-2">
+                                    {showPassword ? <EyeOff /> : <Eye />}
+                                </div>
+                            </div>
                         </div>
-                        <Button type="submit" className='w-full'>Entrar</Button>
-                        <div className='text-center mt-4'>
-                            <p>Need to create an account?
-                                <a href="/signup" className='text-blue-500'> Sign Up</a>
-                            </p>
-                        </div>
-                    </form>
+                        <Button className="mt-4 w-full">Login</Button>
+                    </div>
+                    <p className='pt-8 text-gray-500'>Need to create an account ? <a href="/register" className="text-black underline">Sign Up</a></p>
                 </div>
             </div>
         </div>
